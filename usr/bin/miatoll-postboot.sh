f534258f23a757302ac16a856cd496a058832353
Bardia Moshiri <fakeshell@bardia.tech>
2023-07-08 19:36:43 -0400

    miatoll-postboot: enable DT2W

--- usr/bin/miatoll-postboot.sh
+++ usr/bin/miatoll-postboot.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-set -e
-
 if [ -z "${LAUNCHED_BY_SYSTEMD}" ]; then
     echo "This script is automatically executed at boot by systemd. Quiting.."
     exit 1
 fi
 
 # Enable wlan modem
-echo ON | tee /dev/wlan
+echo ON > /dev/wlan
 
 # Disable lowmemorykiller
-echo 0 | tee /sys/module/lowmemorykiller/parameters/*_lmk
+echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+echo 0 > /sys/module/lowmemorykiller/parameters/enable_lmk
 
 # Set swappiness value to 90
-echo 90 | tee /proc/sys/vm/swappiness
+echo 90 > /proc/sys/vm/swappiness
+
+# Enable DT2W
+echo 1 > /sys/touchpanel/double_tap
