202858c028fe86d52c8d3603a0cf3900f51d7868
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-06-04 11:54:29 +0200

    miatoll-postboot: Remove double tap configuration

--- usr/bin/miatoll-postboot.sh
+++ usr/bin/miatoll-postboot.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-[ -f /etc/miatoll.conf ] && . /etc/miatoll.conf
-
 if [ -z "${LAUNCHED_BY_SYSTEMD}" ]; then
     echo "This script is automatically executed at boot by systemd. Quiting.."
     exit 1
@@ -13,13 +11,6 @@ echo ON > /dev/wlan
 # Disable lowmemorykiller
 echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
 echo 0 > /sys/module/lowmemorykiller/parameters/enable_lmk
-
-# Set swappiness value to 90. => handled by power_saver kernel module
-# echo 90 > /proc/sys/vm/swappiness
-
-# Enable DT2W
-[ -z $TOUCHPANEL_DOUBLE_TAP ] && TOUCHPANEL_DOUBLE_TAP=1
-echo  $TOUCHPANEL_DOUBLE_TAP > /sys/touchpanel/double_tap
 
 # Set pulseaudio default settings to prevent crackling (see /etc/security/limits.d/pulse.conf)
 sed -i '
