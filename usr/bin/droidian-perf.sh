d34c604d31074589dea52ebb2b3eddf855b9c036
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-06-21 17:17:48 +0200

    services: Rename droidian-perf.service to miatoll-preboot.service

--- usr/bin/droidian-perf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p /sys/fs/cgroup/schedtune
-mount -t cgroup -o schedtune stune /sys/fs/cgroup/schedtune # needs schedtune in kernel
---
+++ usr/bin/miatoll-preboot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p /sys/fs/cgroup/schedtune
+mount -t cgroup -o schedtune stune /sys/fs/cgroup/schedtune # needs schedtune in kernel
+
+for i in background system-background
+do
+    mkdir -p /dev/cpuset/$i
+    echo 0-3 > /dev/cpuset/$i/cpus
+    echo 0   > /dev/cpuset/$i/mems
+done
+
+for i in top-app foreground camera-daemon
+do
+    mkdir -p /dev/cpuset/$i
+    echo 0-7 > /dev/cpuset/$i/cpus
+    echo 0   > /dev/cpuset/$i/mems
+done
