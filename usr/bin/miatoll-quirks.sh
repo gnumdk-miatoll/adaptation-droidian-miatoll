835535e5b9b4676499bfcf0378126c022a5e53ed
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-06-25 07:15:39 +0200

    miatoll-quirks: Remove camera workaround

--- usr/bin/miatoll-quirks.sh
+++ usr/bin/miatoll-quirks.sh
@@ -1,4 +1,1 @@
-#!/bin/sh
-waitforservice init.svc.camera_service
-timeout 5 gst-launch-1.0 droidcamsrc camera_device=0 mode=2 ! tee name=t t. ! queue ! video/x-raw, width=1920, height=1080 ! videoconvert ! videoflip video-direction=auto ! fakesink
-timeout 5 gst-launch-1.0 droidcamsrc camera_device=0 mode=2 ! tee name=t t. ! queue ! video/x-raw, width=1920, height=1080 ! videoconvert ! videoflip video-direction=auto ! fakesink
+#!/bin/bash
